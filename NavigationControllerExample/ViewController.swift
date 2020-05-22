8a9fde18cd822371fd4163a5f63279a1978ab2af
James Kong <fishkingsin@gmail.com>
2020-05-22 14:16:18 +0800

    Initial Commit

---
+++ NavigationControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NavigationControllerExample
+//
+//  Created by James Kong on 22/5/2020.
+//  Copyright © 2020 James Kong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
